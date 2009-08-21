4eef8c6de37382612233ec87439e82d9291e296a
Chris Adams <chris@improbable.org>
2009-08-20 21:15:28 -0400

    Better html_clean/sanitize imports
    Now you can simply do something like this:
    
    >>> from django_wysiwyg import sanitize_html
    >>> sanitize_html('foobar<p style="color:red; remove:me; background-image: url(http://example.com/test.php?query_string=bad);">adf<script>alert("Uhoh!")</script><i></p>abc</i>')
    
    __init__.py has less code but it dynamically tries first the html5lib
    and pytidylib imports. In either case the non-overriden import will
    raise an ImportError when you call it so you aren't pressured to install
    either library if you don't plan to validate.

--- django_wysiwyg/__init__.py
+++ django_wysiwyg/__init__.py
@@ -1,54 +1,17 @@
-def clean_html5lib(input):
-    """
-    Takes an HTML fragment and processes it using html5lib to ensure that the HTML is well-formed.
+def clean_html():
+    raise ImportError("clean_html requires html5lib or pytidylib")
 
-    >>> clean_html("<p>Foo<b>bar</b></p>")
-    u'<p>Foo<b>bar</b></p>'
-    >>> clean_html("<p>Foo<b>bar</b><i>Ooops!</p>")
-    u'<p>Foo<b>bar</b><i>Ooops!</i></p>'
-    >>> clean_html('<p>Foo<b>bar</b>& oops<a href="#foo&bar">This is a <>link</a></p>')
-    u'<p>Foo<b>bar</b>&amp; oops<a href=#foo&amp;bar>This is a &lt;&gt;link</a></p>'
-    """
-    from html5lib import treebuilders, treewalkers, serializer
-    from html5lib.filters import cleanr
-
-    p = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
-
-    dom_tree = p.parseFragment(input)
-
-    walker = treewalkers.getTreeWalker("dom")
-
-    stream = walker(dom_tree)
-
-    s = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False, clean=True)
-    return "".join(s.serialize(stream))
-
-def sanitize_html5lib(input):
-    p = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer)
-    dom_tree = p.parseFragment(input)
-
-    walker = treewalkers.getTreeWalker("dom")
-
-    stream = walker(dom_tree)
-
-    s = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False, clean=True)
-    return "".join(s.serialize(stream))
-
-def clean_pytidylib(input):
-    (cleaned_html, warnings) = tidylib.tidy_document(html)
-    return cleaned_html
+def sanitize_html():
+    raise ImportError("sanitize_html requires html5lib")
+    
 
 try:
     import html5lib
-    clean_html = clean_html5lib
-    sanitize_html = sanitize_html5lib
+    from utils import clean_html5lib as clean_html
+    from utils import sanitize_html5lib as sanitize_html
 except ImportError:
     try:
         import tidylib
-        clean_html = clean_tidylib
+        from utils import clean_tidylib as clean_html
     except ImportError:
         pass
-
-if not "clean_html" in locals():
-    import logging
-    logging.error("Unable to import html5lib or tidylib")
---
+++ django_wysiwyg/utils.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+def clean_html(*args, **kwargs):
+    raise ImportError("clean_html requires html5lib or pytidylib")
+
+def sanitize_html(*args, **kwargs):
+    raise ImportError("sanitize_html requires html5lib")
+    
+def clean_html5lib(input):
+    """
+    Takes an HTML fragment and processes it using html5lib to ensure that the HTML is well-formed.
+
+    >>> clean_html5lib("<p>Foo<b>bar</b></p>")
+    u'<p>Foo<b>bar</b></p>'
+    >>> clean_html5lib("<p>Foo<b>bar</b><i>Ooops!</p>")
+    u'<p>Foo<b>bar</b><i>Ooops!</i></p>'
+    >>> clean_html5lib('<p>Foo<b>bar</b>& oops<a href="#foo&bar">This is a <>link</a></p>')
+    u'<p>Foo<b>bar</b>&amp; oops<a href=#foo&amp;bar>This is a &lt;&gt;link</a></p>'
+    """
+    from html5lib import treebuilders, treewalkers, serializer, sanitizer
+
+    p = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
+
+    dom_tree = p.parseFragment(input)
+
+    walker = treewalkers.getTreeWalker("dom")
+
+    stream = walker(dom_tree)
+
+    s = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False)
+    
+    return "".join(s.serialize(stream))
+
+def sanitize_html5lib(input):
+    """
+    
+    >>> sanitize_html5lib("foobar<p>adf<i></p>abc</i>")
+    u'foobar<p>adf<i></i></p><i>abc</i>'
+    >>> sanitize_html5lib('foobar<p style="color:red; remove:me; background-image: url(http://example.com/test.php?query_string=bad);">adf<script>alert("Uhoh!")</script><i></p>abc</i>')
+    u'foobar<p style="color: red;">adf&lt;script&gt;alert("Uhoh!")&lt;/script&gt;<i></i></p><i>abc</i>'
+    """
+    from html5lib import treebuilders, treewalkers, serializer, sanitizer
+    
+    p = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer, tree=treebuilders.getTreeBuilder("dom"))
+    dom_tree = p.parseFragment(input)
+
+    walker = treewalkers.getTreeWalker("dom")
+
+    stream = walker(dom_tree)
+
+    s = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False)
+    return "".join(s.serialize(stream))
+
+def clean_pytidylib(input):
+    (cleaned_html, warnings) = tidylib.tidy_document(html)
+    return cleaned_html
+    
+try:
+    import html5lib
+    clean_html,  sanitize_html = clean_html5lib, sanitize_html5lib
+except ImportError:
+    try:
+        import tidylib
+        clean_html = clean_tidylib
+    except ImportError:
+        pass
+        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
