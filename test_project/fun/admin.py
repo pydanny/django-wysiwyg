d4b386c4402772eb5bfc75ece8e1a78f57f0fe01
Chris Adams <chris@improbable.org>
2012-03-14 09:36:11 -0700

    test_project: added an example of a YUI config customization

--- test_project/fun/admin.py
+++ test_project/fun/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Playground
+from .models import Playground, FancyPlayground
+
 
 class CartWheelAdmin(admin.ModelAdmin):
-    change_form_template = 'fun/admin/change_form.html'
+    change_form_template = 'fun/admin/playground/change_form.html'
+
+
+class FancyCartWheelAdmin(admin.ModelAdmin):
+    change_form_template = 'fun/admin/fancyplayground/change_form.html'
+
 
 admin.site.register(Playground, CartWheelAdmin)
+admin.site.register(FancyPlayground, FancyCartWheelAdmin)
--- test_project/fun/models.py
+++ test_project/fun/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
-class Playground(models.Model):
 
+class Playground(models.Model):
+    body = models.TextField()
+
+
+class FancyPlayground(models.Model):
+    title = models.CharField(max_length=100)
     body = models.TextField()
