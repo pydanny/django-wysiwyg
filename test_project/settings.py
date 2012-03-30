5cd19ea7a80dd5c181ee5f1ac750c99d2c3c0206
Chris Adams <chris@improbable.org>
2012-03-30 16:59:37 -0300

    Update test project settings to use newer Django 1.2+ template loader syntax

--- test_project/settings.py
+++ test_project/settings.py
@@ -56,11 +56,8 @@ ADMIN_MEDIA_PREFIX = '/admin_media/' # Set this so our files can use /media/
 SECRET_KEY = 'v!y*k!pa947m205&g*ih*a20651l=-gagc_$ntdjt9$_g85c=!'
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
-)
+TEMPLATE_LOADERS = ('django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader')
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
