0ae82999066c348149310680d323034c67f32d75
Daniel Greenfeld <danny@eventbrite.com>
2014-07-02 19:04:03 -0700

    prep for 0.7.0

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Internet :: WWW/HTTP :: Browsers",
         "Topic :: Internet :: WWW/HTTP :: Site Management",
         "Topic :: Office/Business :: Office Suites",
